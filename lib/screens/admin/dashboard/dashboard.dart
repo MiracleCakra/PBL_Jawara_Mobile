e13bef51f17b2d37ab84135c97bc31274196cb25
sirfara <aratihsirf@gmail.com>
2025-11-13 16:56:36 +0700

    dashboard marketplace

--- lib/screens/admin/dashboard/dashboard.dart
+++ lib/screens/admin/dashboard/dashboard.dart
@@ -4,6 +4,7 @@ import 'package:jawara_pintar_kel_5/providers/admin_dashboard_provider.dart';
 import 'package:jawara_pintar_kel_5/screens/admin/dashboard/kegiatan.dart';
 import 'package:jawara_pintar_kel_5/screens/admin/dashboard/kependudukan.dart';
 import 'package:jawara_pintar_kel_5/screens/admin/dashboard/keuangan.dart';
+import 'package:jawara_pintar_kel_5/screens/admin/dashboard/marketplace.dart';
 import 'package:moon_design/moon_design.dart';
 import 'package:provider/provider.dart';
 
@@ -132,7 +133,7 @@ class _AdminDashboardState extends State<AdminDashboard> {
                     _previousPage = provider.page;
                     provider.page = index;
                   },
-                  children: [Keuangan(), Kegiatan(), Kependudukan()],
+                  children: [Keuangan(), Kegiatan(), Kependudukan(), Marketplace()],
                 ),
               ),
             ],
